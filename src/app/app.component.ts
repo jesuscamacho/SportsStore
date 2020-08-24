0ef5ed802e54ab40ed3d5755b95b69dad31f9cd7
jesuscamacho <camacho.jesusb50@gmail.com>
2020-08-23 20:17:29 -0400

    Installed: - bootstrap - font-awsome  dev only:  - json server  - json web token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app",
+  template: `<div>hiii</div>`,
 })
-export class AppComponent {
-  title = 'SportsStore';
-}
+
+export class AppComponent { }
