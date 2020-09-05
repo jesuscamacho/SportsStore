f6cb201d70b9cb7dfe2f071a4c33c714253eaf8b
jesuscamacho <camacho.jesusb50@gmail.com>
2020-09-05 18:15:48 -0400

    set up angular routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { StoreModule } from './store/store.module';
 import { AppComponent } from './app.component';
+import { StoreComponent } from './store/store.component';
+import { CheckoutComponent } from './store/checkout.component';
+import { CartDetailComponent } from './store/cartDetail.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, StoreModule],
+  imports: [BrowserModule, StoreModule,
+    RouterModule.forRoot([
+      { path:'store', component:StoreComponent},
+      { path:'cart', component: CartDetailComponent},
+      { path:'checkout', component:CheckoutComponent},
+      { path:'**', redirectTo: '/store'}
+    ])],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
