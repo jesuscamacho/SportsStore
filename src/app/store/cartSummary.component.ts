503e77702ec66d3e8efd5bd2121e1afc772811bf
jesuscamacho <camacho.jesusb50@gmail.com>
2020-09-12 15:25:35 -0400

    added route guard to store, cartDetail, checkout pages

--- src/app/store/cartSummary.component.ts
+++ src/app/store/cartSummary.component.ts
@@ -1,10 +1,12 @@
 import { Component } from "@angular/core";
 import { Cart } from "../model/cart.model";
+import { Router } from "@angular/router";
+
 
 @Component({
     selector: 'cart-summary',
     templateUrl: './cartSummary.component.html'
 })
 export class CartSummaryComponent {
-    constructor(public cart: Cart) {}
+    constructor(public cart: Cart, private router:Router) {}
 }
