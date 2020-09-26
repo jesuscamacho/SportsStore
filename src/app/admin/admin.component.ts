ac1af96e0906a66d6b6afc0f494276450fea27a4
jesuscamacho <camacho.jesusb50@gmail.com>
2020-09-26 15:30:10 -0400

    updated admin module/routes

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,5 +1,16 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthService } from "../model/auth.service";
+
 @Component({
     templateUrl:'./admin.component.html'
 })
-export class AdminComponent {}
+export class AdminComponent {
+
+    constructor(private router: Router, private authService: AuthService) {}
+
+    logout() {
+        this.authService.clear();
+        this.router.navigateByUrl("/");
+    }
+}
---
+++ src/app/admin/productEditor.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+    template:`<div class="bg-info p-2 text-white">s </div>`
+})
+
+export class ProductEditorComponent {}
